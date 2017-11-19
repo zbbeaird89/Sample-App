3695ce18a298c0103427008901d77e3843dc3f0a
Zach Beaird <zbbeaird1989@gmail.com>
2017-11-18 23:52:08 -0600

    Finishes site layout

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title test" do
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+  end
+end
